a6e9b1d185d5e117cc1d46a2529e6a36de205880
Liirus91 <work.liirus@gmail.com>
2025-06-05 08:15:35 +0300

    feat: use json in express

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.post('/auth/login', (req, res) => {
+  res.json({ success: true });
 });
 
 app.listen(4444, (err) => {
