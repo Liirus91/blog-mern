5ea7b9416be91f150f33b90a639572c83fb2101a
Liirus91 <work.liirus@gmail.com>
2025-06-11 07:21:16 +0300

    feat: add mongoose

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:password@cluster0.5dkt98k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+  });
 
 const app = express();
 
