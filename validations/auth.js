1f7ea8a9f3baef58d040955830d4a304dace9ffd
Liirus91 <work.liirus@gmail.com>
2025-06-29 08:07:47 +0300

    refactor: small changes in auth validation

--- validations/auth.js
+++ validations/auth.js
@@ -1,10 +1,14 @@
 import { body } from 'express-validator';
 
-export const registerValidation = [
+export const loginValidation = [
   body('email', 'Invalid email format').isEmail(),
   body('password', 'Password must be at least 5 characters long').isLength({
     min: 5,
   }),
+];
+
+export const registerValidation = [
+  ...loginValidation,
   body('fullName', 'Full name must be at least 3 characters long').isLength({
     min: 3,
   }),
