a383ebf5d316ea6d8e61dab059ecfcc9ad744826
Liirus91 <work.liirus@gmail.com>
2025-07-02 06:38:17 +0300

    feat: finish create in post controller

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,20 @@
+import PostModel from '../models/Post.js';
+
 export const create = async (req, res) => {
   try {
+    const { title, text, tags, imageUrl } = req.body;
+
+    const doc = new PostModel({
+      title,
+      text,
+      tags,
+      user: req.userId,
+      imageUrl,
+    });
+
+    const post = await doc.save();
+
+    res.status(201).json(post);
   } catch (error) {
     console.error('Post creation error: ', error);
     res.status(500).json({ message: 'Internal server error' });
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { registerValidation, loginValidation } from './validations/auth.js';
 import checkAuth from './utils/checkAuth.js';
+import { postCreateValidation } from './validations/post.js';
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose
   .connect(
@@ -24,6 +26,12 @@ app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+//app.get('/posts', PostController.getAll);
+//app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+//app.delete('/posts/:id', PostController.remove);
+//app.patch('/posts/:id', PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
