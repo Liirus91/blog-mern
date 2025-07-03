f1bf5bccd6cdfaeabcf35ea563b82738e708c187
Liirus91 <work.liirus@gmail.com>
2025-07-03 07:28:56 +0300

    feat: add getAll method in PostController

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,15 @@
 import PostModel from '../models/Post.js';
 
+export const getAll = async (_req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec();
+    res.json(posts);
+  } catch (error) {
+    console.error('Error fetching posts: ', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const { title, text, tags, imageUrl } = req.body;
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/auth/register', registerValidation, UserController.register);
 
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-//app.get('/posts', PostController.getAll);
+app.get('/posts', PostController.getAll);
 //app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 //app.delete('/posts/:id', PostController.remove);
