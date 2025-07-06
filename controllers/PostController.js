7b026d7e9feeb20bcf4e17b5353af2ef9be44b70
Liirus91 <work.liirus@gmail.com>
2025-07-06 07:40:56 +0300

    feat: add getOne PostController

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,6 +10,38 @@ export const getAll = async (_req, res) => {
   }
 };
 
+export const getOne = async (_req, res) => {
+  try {
+    const { id } = _req.params;
+    PostModel.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.error('Error updating views count: ', err);
+          return res.status(500).json({ message: 'Internal server error' });
+        }
+
+        if (!doc) {
+          return res.status(404).json({ message: 'Post not found' });
+        }
+
+        res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.error('Error fetching posts: ', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const { title, text, tags, imageUrl } = req.body;
